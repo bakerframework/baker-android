1f1c98aa84dbc4c65c8d6069fea729b95c91317a
hsalazar <hsalazar@giniem.com>
2014-03-25 22:14:49 -0600

    - Removed menu for the modal activity.

--- ABakerProject/ABaker/src/main/java/com/baker/abaker/ModalActivity.java
+++ ABakerProject/ABaker/src/main/java/com/baker/abaker/ModalActivity.java
@@ -92,9 +92,6 @@ public class ModalActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.modal, menu);
         return true;
     }
 
@@ -103,10 +100,6 @@ public class ModalActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
         return super.onOptionsItemSelected(item);
     }
 
