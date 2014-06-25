42e5cd91720149c35e07365d2404d5dc157c6f00
hsalazar <hsalazar@giniem.com>
2014-06-24 18:31:59 -0600

    - Solved problem when extrating files with non UTF-8 encoded names.

--- ABakerProject/ABaker/src/main/java/com/baker/abaker/workers/UnzipperTask.java
+++ ABakerProject/ABaker/src/main/java/com/baker/abaker/workers/UnzipperTask.java
@@ -4,17 +4,16 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import com.baker.abaker.settings.Configuration;
 import com.baker.abaker.client.GindMandator;
+import com.baker.abaker.settings.Configuration;
 
-import java.io.BufferedInputStream;
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 public class UnzipperTask extends AsyncTask<String, Long, String> {
 
@@ -30,20 +29,16 @@ public class UnzipperTask extends AsyncTask<String, Long, String> {
         this.resumed = _resumed;
     }
 
-	public UnzipperTask(Context context, GindMandator mandator, final int taskId) {
+    public UnzipperTask(Context context, GindMandator mandator, final int taskId) {
         this.context = context;
         this.mandator = mandator;
         this.taskId = taskId;
-	}
+    }
 
     @Override
     protected String doInBackground(String... params) {
-        InputStream input;
-        ZipInputStream zipInput;
         try {
-            String zipEntryName;
-
-            Log.d(this.getClass().getName(),"Started unzip process for file " + params[0]);
+            Log.d(this.getClass().getName(), "Started unzip process for file " + params[0]);
 
             // First we create a directory to hold the unzipped files.
             String workingDir = params[0].substring(0, params[0].lastIndexOf("/")) + File.separator;
@@ -56,51 +51,69 @@ public class UnzipperTask extends AsyncTask<String, Long, String> {
             }
 
             if (containerDir.mkdirs()) {
-                input = new FileInputStream(params[0]);
-
                 workingDir = workingDir + params[1] + File.separator;
-                zipInput = new ZipInputStream(new BufferedInputStream(input));
-                ZipEntry zipEntry;
-                byte[] buffer = new byte[1024];
-                int read;
-
-                while ((zipEntry = zipInput.getNextEntry()) != null) {
-
-                    zipEntryName = zipEntry.getName();
-                    Log.d(this.getClass().toString(), "Unzipping entry " + zipEntryName);
-                    if (zipEntry.isDirectory()) {
-                        File innerDirectory = new File(workingDir + zipEntryName);
-                        innerDirectory.mkdirs();
-                        continue;
-                    }
-
-                    FileOutputStream output = new FileOutputStream(workingDir + zipEntryName);
-                    while ((read = zipInput.read(buffer)) != -1) {
-                        output.write(buffer, 0, read);
-                    }
-
-                    output.close();
-                    zipInput.closeEntry();
-                }
-
-                zipInput.close();
-
-                // Delete the zip file
-                File zipFile = new File(params[0]);
-                zipFile.delete();
+                this.extract(params[0], workingDir);
             } else {
                 Log.e(this.getClass().getName(), "Could not create the package directory");
                 //TODO: Notify the user
                 return null;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException ex) {
+            Log.e(this.getClass().getName(), "Error unzipping the issue.", ex);
             return null;
         }
 
         Log.d(this.getClass().getName(), "Unzip process finished successfully.");
-		return "SUCCESS";
-	}
+        return "SUCCESS";
+    }
+
+    private void extract(final String inputFile, final String outputDir) throws IOException  {
+        FileInputStream fileInputStream = null;
+        ZipArchiveInputStream zipArchiveInputStream = null;
+        FileOutputStream fileOutputStream = null;
+        try {
+
+            Log.d(this.getClass().getName(), "Will extract " + inputFile + " to " + outputDir);
+
+            byte[] buffer = new byte[8192];
+            fileInputStream = new FileInputStream(inputFile);
+
+            // We use null as encoding.
+            zipArchiveInputStream = new ZipArchiveInputStream(fileInputStream, null, true);
+            ArchiveEntry entry;
+            while ((entry = zipArchiveInputStream.getNextEntry()) != null) {
+                Log.d(this.getClass().getName(), "Extracting entry " + entry.getName());
+                File file = new File(outputDir, entry.getName());
+                if (entry.isDirectory()) {
+                    file.mkdirs();
+                } else {
+                    file.getParentFile().mkdirs();
+                    fileOutputStream = new FileOutputStream(file);
+                    int bytesRead;
+                    while ((bytesRead = zipArchiveInputStream.read(buffer, 0, buffer.length)) != -1)
+                        fileOutputStream.write(buffer, 0, bytesRead);
+                    fileOutputStream.close();
+                    fileOutputStream = null;
+                }
+            }
+            // Delete the zip file
+            File zipFile = new File(inputFile);
+            zipFile.delete();
+
+        } finally {
+            try {
+                zipArchiveInputStream.close();
+                fileInputStream.close();
+                if (fileOutputStream != null) {
+                    fileOutputStream.close();
+                }
+            } catch (NullPointerException ex) {
+                Log.e(this.getClass().getName(), "Error closing the file streams.", ex);
+            } catch (IOException ex) {
+                Log.e(this.getClass().getName(), "Error closing the file streams.", ex);
+            }
+        }
+    }
 
     @Override
     protected void onProgressUpdate(Long... progress) {
